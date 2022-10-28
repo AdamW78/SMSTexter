ed3d417b5ac3b79d0817cfad972bb6c2f6d0ca94
Adam W <Code.AdamW78@gmail.com>
2022-10-28 14:58:32 -0400

    Optimized loops and reformatted

--- CarrierUtils/find_cell_carrier.py
+++ CarrierUtils/find_cell_carrier.py
@@ -1,22 +1,37 @@
+"""
+Uses either Twilio API or locally cached cell carrier value to return cell carrier
+"""
 from twilio.rest import Client
 import constants
 from Cache import cache_cell_carrier
 
 
 def get_carrier(number):
+    """
+    Method used for fetching cell carrier for supplied number
+
+    :param number: String phone number for which cell carrier will be returned
+    :return: String cell carrier found either locally or using Twilio API
+    """
+    # Check if String number is cached locally
     cache_result = cache_cell_carrier.is_cached(number)
+    # Check if the cache lookup result is a string
     if isinstance(cache_result, str):
+        # It is a string, found cell carrier locally, return cell carrier string
         return cache_result
-    url = f"https://lookups.twilio.com/v2/PhoneNumbers/{number}"
+    # Values below stored in constants.py - untracked file
     account_sid = constants.TWILIO_SID
     auth_token = constants.AUTH_TOKEN
+    # Code below sourced directly from Twilio documentation
     client = Client(account_sid, auth_token)
     phone_number = client.lookups \
         .v1 \
         .phone_numbers(number) \
         .fetch(type=['carrier'])
     cell_carrier = phone_number.carrier['name']
+    # Cache cell carrier Twilio returns locally in JSON file
     cache_cell_carrier.cache(number, cell_carrier)
     if constants.DEBUG:
         print(f"Received response from Twilio for {number} - Carrier: {cell_carrier}")
+    # Return the cell carrier found by the Twilio API
     return cell_carrier
--- DictUtils/carrier_dict_loader.py
+++ DictUtils/carrier_dict_loader.py
@@ -1,35 +1,59 @@
+"""
+Module used for importing and using carrier-dict.csv
+"""
 import csv
 import requests
 import constants
 from DictUtils import dict_checker, dict_reader
 
 
-def fetch_carrier_dictionary_local() -> dict:
+def __fetch_carrier_dictionary_local() -> dict:
+    """
+    Function used for fetching local carrier dictionary file
+
+    :return: dictionary object created by reading the file
+    """
     carrier_dict_file = dict_checker.has_csv_dict()
     return dict_reader.read(carrier_dict_file)
 
 
-def fetch_carrier_dictionary() -> dict:
+def __fetch_carrier_dictionary() -> dict:
+    """
+    Function used for fetching cell carrier dictionary from GitHub
+
+    :return: Dictionary file newly-stored locally
+    """
     # Fetch dictionary of cell carriers from GitHub
     response = requests.get(constants.CARRIER_DICT_URL)
     # Create list object line-by-line from remote dictionary file
     lines = list(response.iter_lines())
-    #
-    for i in range(len(lines)):
+    # Iterate through all the lines in the dictionary and convert them to strings
+    for i in enumerate(lines):
         lines[i] = str(lines[i])
+    # Fetch path to dictionary file from constants
     dict_patch = constants.LOCAL_DICT_PATCH
-    with open(dict_patch, 'w') as csvfile:
+    # Open dictionary file and write to it
+    with open(dict_patch, 'w', encoding='UTF-8') as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
+        # Format each line and output to csv file
         for line in lines:
             line = line[2:len(line)-1]
             write_list = line.split(',')
             writer.writerow(write_list)
+    # Return dictionary object created from locally stored dictionary newly created
     return dict_reader.read(dict_patch)
 
 
-def carrier_dictionary():
+def carrier_dictionary() -> dict:
+    """
+    Function used to return carrier dictionary
+
+    :return: Dict object containing cell carriers and their text-to-mail addresses
+    """
+    # If in DEBUG mode or if local dict does NOT exist, fetch from GitHub
     if constants.DEBUG or (dict_checker.has_csv_dict() == ""):
-        _carrier_dictionary = fetch_carrier_dictionary()
-    else:
-        return fetch_carrier_dictionary_local()
-    return _carrier_dictionary
+        _carrier_dictionary = __fetch_carrier_dictionary()
+        # Return fetched dict object
+        return _carrier_dictionary
+    # Local dictionary file already exists, fetch, turn into dict object, and return it
+    return __fetch_carrier_dictionary_local()
